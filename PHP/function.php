8bc4d6696a7120d6b6e867d971a4004ad3f781f0
Algraud <algraud@gmail.com>
2017-03-26 00:09:20 -0500

    functions choose Base, Subs, Types

--- PHP/function.php
+++ PHP/function.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'config.php';
+include_once 'PHP/db.php';
  
 function sec_session_start() {
     $session_name = 'sec_session_id';   // Set a custom session name
@@ -80,5 +81,79 @@ function login($vardas, $password, $db) {
             // No user exists.
             return false;
         }
+    }
+}
+function itemDrop($drop){
+    if($drop=="all"){
+        //$check=rand(1,1);
+        $check=2;
+        if($check==2){
+            $drop="armor"
+        }
+    }
+    if($drop=="armor"){
+        //get info from db
+        $Base = mysqli_query($db,"SELECT * FROM basearmor Order by RAND() Limit     1");
+        $Base = mysqli_fetch_row($Base);
+        $Sub = mysqli_query($db,"SELECT * FROM subfixwep Order by RAND() Limit  1");
+        $Sub = mysqli_fetch_row($Sub);
+        $Sub2 = mysqli_query($db,"SELECT * FROM subfixwep Order by RAND() Limit     1");
+        $Sub2 = mysqli_fetch_row($Sub2);
+        $Type = mysqli_query($db,"SELECT * FROM types Order by RAND() Limit     1");
+        $Type = mysqli_fetch_row($Type);
+        $Type2 = mysqli_query($db,"SELECT * FROM types Order by RAND() Limit    1");
+        $Type2 = mysqli_fetch_row($Type2);
+
+        $Enchant = mysqli_query($db,"SELECT * FROM enchantdrop");
+
+        //Nulify vars
+        $nameType = "";    
+        $iLVL = 0;
+        $armor = 0; //was dmg, delete comment before finnish
+        $typeMulti = 0; //procental upgrade
+        $elvl = 0; //???
+
+        /*
+        $n1 = $MLVL/1;
+        $n3 = $MLVL/1.9;
+        $n4 = $MLVL/2.4;
+        $r = $n1 *2.5;
+        */
+        //rng stuff for 
+        $rngSub = rand(1,99-($MLVL/1.9));
+        $rngSub2 = rand(1,150-($MLVL/2.4));
+        $rngType = rand(0,500-($MLVL*2.5));
+
+
+        $nameBase = $Base[1]; //takes the base name for the Armor
+        $iLVL = $Base[3]; //base level
+        $armor = $Base[2]; //base armor
+        if ($rngSub < 20){  
+            $nameSub ="of $Sub[1]";
+            $armor += $Sub[3]/2;
+            $iLVL += $Sub[2];
+            if ($rngSub2 < 30){
+                $nameSub2 ="and $Sub2[1]";
+                $armor += $Sub2[3]/2;
+                $ILVL += $Sub2[2];
+            }
+        }
+        if ($rngType < $Type[2]){
+            $nameType ="$Type[1]";
+            $color = "$Type[4]";
+            $typeMulti = $armor * $Type[3] / 100;
+            $Armor += $typeMulti/2;
+            $typeMulti = $iLVL * $Type[3] / 100;
+            $iLVL += $typeMulti;
+        }
+        else if ($rngType < $Type2[2]){
+            $nameType ="$Type2[1]";
+            $color = "$Type2[4]";
+            $typeMulti = $armor * $Type2[3] / 100;
+            $armor += $typeMulti/2;
+            $typeMulti = $iLVL * $Type2[3] / 100;
+            $ILVL += $typeMulti;}
+
+
     }
 }

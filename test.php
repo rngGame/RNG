8561196287a6c3a841387630c71577841353f19b
Paulius <paulius.ruplys9@gmail.com>
2017-04-06 13:56:51 +0300

    Test 0.9.5

--- test.php
+++ test.php
@@ -2,21 +2,7 @@
 session_start();
 ob_start();
 ?>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js">  
-</script>
-<script type="text/javascript">
-$(document).ready(function(){
-    $("button").click(function(){
-        $.post("PHP/point.php",
-        {
-          name: "STR",
-        },
-        function(data,status){
-            alert("Data: " + data + "\nStatus: " + status);
-        });
-    });
-});
-</script>
+
 <?php
 
 include_once 'PHP/db.php';
@@ -24,24 +10,17 @@ include_once 'PHP/db.php';
 $User = $_SESSION["User"];
 
 $ACC = mysqli_query($db,"SELECT * FROM characters where user = '$User' ");
-$ACC = mysqli_fetch_row($ACC);
+$row = mysqli_fetch_assoc($ACC);
 
 
 
-?>
-<button>Send an HTTP POST request to a page and get the result back</button>
 
+if (!$row) {
+        echo 'MySQL Error: ' . mysqli_error();
+        exit;
+    }
 
-<div id="result"></div>
-
-
-
-<?php
-
-
-if(isset($_POST['submit'])){
-echo "HELLO";
-}
+echo $row["HP"];
 
 
 

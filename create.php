747af9d598c383ef274ddab3948114e1d73975be
Kompas9 <paulius.ruplys9@gmail.com>
2017-04-07 21:20:40 +0300

    Fixed account creation for 0.9.5

--- create.php
+++ create.php
@@ -33,7 +33,7 @@ $count4 = mysqli_num_rows($result4);
 if(!$count==1){
 	
     if (isset($_POST['username']) && isset($_POST['password'])){
-        $query = "INSERT INTO account (User, Psw, Theme) VALUES ('$username', '$hashed_password', 'meniu')";
+        $query = "INSERT INTO account (User, Psw, Theme) VALUES ('$username', '$hashed_password', 'meniu_dark')";
         $result = mysqli_query($db,$query);}
 		$query2 = "INSERT INTO Points (User, Free, STR, INTE) VALUES ('$username', '1', '0', '0')";
         $result = mysqli_query($db,$query2);
@@ -45,51 +45,33 @@ if(!$count==1){
 if ($count2 == 1){
 	 }
 else {
-$order2 = "INSERT INTO weapondrops
-	   (HASH, Name, Type, Color, ilvl, pmin, pmax, cryt, ats, mmin, mmax, hc, skill, effect, efstat, plus)
+$order2 = "INSERT INTO DropsWep
+	   (HASH, Name, Rarity, ilvl, pmin, pmax, cryt, mmin, mmax, HitChanse, skill, effect, efstat, plus)
 	  VALUES
-	   ('0001', 'Wooden Sword', '', '', '1', '1', '5', '1', '1', '1', '5', '90', '', '', '', '0')";
+	   ('0001', 'Wooden Sword', '', '1', '1', '5', '1', '1', '5', '90', '', '', '', '0')";
 	   $result = mysqli_query($db, $order2);	
-	$order3 = "INSERT INTO drops
-	   (ID, Name, Type, ilvl, dmg, color, HASH)
-	  VALUES
-	   ('2', 'Wooden armor','','1','1', '', '0002')";
-	   $result = mysqli_query($db, $order3);		
+	   		
 	   }   
 	   
-if ($count3 == 1)
-	{}
-else {
-		$order4 = "INSERT INTO dropst
-	  (NAME,LVL,DMG,HP, ARMOR, XP, HASH, Type, Color)
-	 	VALUES
-	   ('None','0','0','0','0','1','0003','','')";
-	  	$result = mysqli_query($db, $order4);	
-		
-}
+$order3 = "INSERT INTO Equiped
+(User, Part, HASH, Equiped)
+VALUES
+('$username', 'WEP', '0001', '1')";	
 
-if ($count4 == 1)
-	{}
-else {
-		$order5 = "INSERT INTO Gems
-	  (Name, HASH, Type, Color, lvl, Stat1)
-	 	VALUES
-	   ('None', '0004', 'None', '000000', '1', '0')";
-	  	$result = mysqli_query($db, $order5);	
-		
-}
+$result = mysqli_query($db, $order3);	
 
 
 $order2 = "INSERT INTO characters
 	   (User, wep_h, HP, LVL, Cash, XP, Arm_h, Tali_h, Class, Rank, Gem_h, Shards, Speed)
 	  VALUES
-	   ('$username','0001','5','1', '100', '1', '0002', '0003', '0', '1000', '0004', '0', '100')";
+	   ('$username','','5','1', '100', '1', '', '', '0', '1000', '0004', '0', '100')";
 	   
 $result = mysqli_query($db, $order2);		
 			
 			
 			
 			header("location:index.php");
+			die();
         }
 else{
 		echo "Acount already created";

d9600fdeb25305995d292df89d250d44b08c0c66
Kompas9 <paulius.ruplys9@gmail.com>
2017-04-17 18:00:51 +0300

    Pre 0.9.6

--- testing.php
+++ testing.php
@@ -1,18 +1,25 @@
 <?php
+session_start();
+
+error_reporting(0);
+
+include_once 'PHP/db.php';
+$User = $_SESSION["User"];
+include_once 'PHP/function.php';
 //button
 
 
 //test functions
-function testItemDrop($db){
-	for($mlvl=1;$mlvl <=5000;$mlvl++){
-		list($HASH, $weaponName, $weaponType, $weaponLVL, $weaponPhysMin, $weaponPhysMax, $weaponCrit, $weaponMagMin, $weaponMagMax, $weaponHit, $weaponSkill, $weaponEffect, $weaponEffectChance) = itemDrop($db,,"weapon",$MLVL, "LVL DESC", "Bonus DESC" );
-		list($HASH, $armorLVL, $armorName, $armorType, $valueArmorP, $valueArmorM, $armorPart, $armorAbsorb) = itemDrop($db,,"armor",$MLVL, "LVL DESC", "Bonus DESC" );
-		list($HASH, $accesoryPart, $accesoryName, $accesoryType, $accesoryLVL, $accesoryAbsorb, $hpBonus, $xpBonus, $dmgBonus) = itemDrop($db,,"talisman",$MLVL, "LVL DESC", "Bonus DESC" );
+//function testItemDrop($db){
+	for($MLVL=400;$MLVL <=500;$MLVL++){
+		list($HASH, $weaponName, $weaponType, $weaponLVL, $weaponPhysMin, $weaponPhysMax, $weaponCrit, $weaponMagMin, $weaponMagMax, $weaponHit, $weaponSkill, $weaponEffect, $weaponEffectChance) = itemDrop($db, $User, "weapon", $MLVL);
+		list($HASH, $armorLVL, $armorName, $armorType, $valueArmorP, $valueArmorM, $armorPart, $armorAbsorb) = itemDrop($db, $User, "armor", $MLVL);
+		list($HASH, $accesoryPart, $accesoryName, $accesoryType, $accesoryLVL, $accesoryAbsorb, $hpBonus, $xpBonus, $dmgBonus) = itemDrop($db, $User, "talisman", $MLVL);
 		echo "_______________________<br>
-		Items for Monster LVL $mlvl <br>
+		<b>Items for Monster LVL $MLVL</b> <br>
 		Weapon: $weaponLVL $weaponName ($weaponType) $weaponPhysMin ~ $weaponPhysMax, $weaponCrit%, $weaponMagMin ~ $weaponMagMax, $weaponHit% | $weaponSkill, $weaponEffect, $weaponEffectChance %<br>
 		Armor: $armorLVL $armorName ($armorType) valueArmorP / $valueArmorM , $armorPart , $armorAbsorb %<br>
 		Accesorry: $accesoryLVL $accesoryName ($accesoryType) $accesoryPart $accesoryAbsorb%, $hpBonus%, $xpBonus%, $dmgBonus%<br>
 		________________";
 	}
-}
+

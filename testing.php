b96043a8ffd92c3c3cf9ab92a73b08ed68c7e0f6
Kompas9 <paulius.ruplys9@gmail.com>
2017-04-17 21:21:47 +0300

    Revert "Pre 0.9.9"
    This reverts commit c5688462ed2e83fa584fe1f091218f78682bd5ef.

--- testing.php
+++ testing.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-set_time_limit(60);
 error_reporting(0);
 
 include_once 'PHP/db.php';
@@ -12,66 +11,15 @@ include_once 'PHP/function.php';
 
 //test functions
 //function testItemDrop($db){
-	
-	$WEPminLVL=1000000000;
-	$ARMminLVL=1000000000;
-	$ACSminLVL=1000000000;
-	
-	$startTime=time();
-	$i = 0;
-	
-	$MLVL = 325; //minimum lvl
-	$MMAX = 330; //maximum lvl
-	
-	
-	
-	
-	while($MLVL <=$MMAX){
-		$MLVL++;
+	for($MLVL=400;$MLVL <=500;$MLVL++){
 		list($HASH, $weaponName, $weaponType, $weaponLVL, $weaponPhysMin, $weaponPhysMax, $weaponCrit, $weaponMagMin, $weaponMagMax, $weaponHit, $weaponSkill, $weaponEffect, $weaponEffectChance) = itemDrop($db, $User, "weapon", $MLVL);
 		list($HASH, $armorLVL, $armorName, $armorType, $valueArmorP, $valueArmorM, $armorPart, $armorAbsorb) = itemDrop($db, $User, "armor", $MLVL);
 		list($HASH, $accesoryPart, $accesoryName, $accesoryType, $accesoryLVL, $accesoryAbsorb, $hpBonus, $xpBonus, $dmgBonus) = itemDrop($db, $User, "talisman", $MLVL);
-		/*echo "_______________________<br>
+		echo "_______________________<br>
 		<b>Items for Monster LVL $MLVL</b> <br>
-		Weapon: $weaponLVL $weaponName ($weaponType) PDMG - $weaponPhysMin ~ $weaponPhysMax, MDMG - $weaponMagMin ~ $weaponMagMax, CRIT - $weaponCrit%, HS - $weaponHit% BONUS - $weaponSkill, $weaponEffect, $weaponEffectChance %<br>
-		Armor: $armorLVL $armorName ($armorType) $armorPart, DEF - $valueArmorP / $valueArmorM, APS - $armorAbsorb %<br>
-		Accesorry: $accesoryLVL $accesoryName ($accesoryType) $accesoryPart APS - $accesoryAbsorb%, HP - $hpBonus%, XP - $xpBonus%, DMG -$dmgBonus%<br>
-		________________";*/
-		
-		//MAX
-		if ($WEPMAXLVL < $weaponLVL){
-		$WEPMAXLVL = $weaponLVL;
-		}
-		if ($ARMMAXLVL < $armorLVL){
-		$ARMMAXLVL = $armorLVL;
-		}
-		if ($ACSMAXLVL < $accesoryLVL){
-		$ACSMAXLVL = $accesoryLVL;
-		}
-		
-		//MIN
-		if ($WEPminLVL > $weaponLVL){
-		$WEPminLVL = $weaponLVL;
-		}
-		if ($ARMminLVL > $armorLVL){
-		$ARMminLVL = $armorLVL;
-		}
-		if ($ACSminLVL > $accesoryLVL){
-		$ACSminLVL = $accesoryLVL;
-		}
-		//if (time()-$startTime>5) break;
-		$i += 1;
-}
-$MLVLS = $MLVL - $i;
-$MLVL = $MLVL - 1;
-$i -=  1;
-echo"<br><br><br><b>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</b><br>
-	Final Mob Raange: $MLVLS - $MLVL<br>
-	How many lvl passed: $i <br>
-	Weapon ILVL: $WEPminLVL - $WEPMAXLVL<br>
-	Armor ILVL: $ARMminLVL - $ARMMAXLVL<br>
-	Aacsesorys ILVL: $ACSminLVL - $ACSMAXLVL<br>
-	<b>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</b><br>
-	";
-?>
+		Weapon: $weaponLVL $weaponName ($weaponType) $weaponPhysMin ~ $weaponPhysMax, $weaponCrit%, $weaponMagMin ~ $weaponMagMax, $weaponHit% | $weaponSkill, $weaponEffect, $weaponEffectChance %<br>
+		Armor: $armorLVL $armorName ($armorType) valueArmorP / $valueArmorM , $armorPart , $armorAbsorb %<br>
+		Accesorry: $accesoryLVL $accesoryName ($accesoryType) $accesoryPart $accesoryAbsorb%, $hpBonus%, $xpBonus%, $dmgBonus%<br>
+		________________";
+	}
 

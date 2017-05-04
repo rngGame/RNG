16fb14d39ad1340ab6599e47de33c2a6985c5c1d
Paulius <paulius.ruplys9@gmail.com>
2017-05-04 15:09:55 +0300

    0.9.7

---
+++ examp.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+ob_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>World of RNG</title>
+<?php
+echo "<link rel='stylesheet' type='text/css' href='css/meniu_dark.css'>";
+?>
+<link rel="icon" href="favicon.png">
+</head>
+<body>
+<header>
+</header>
+<?php
+
+echo "<br><br><div class='Kompas'>HI DANGE~</div>";
+
+?>
